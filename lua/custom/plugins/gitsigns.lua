bb53bdbd5e9e7a67c72668946817bd6b8275dc7d
Tom <rache.bartmoss@toms-macbook-pro.home>
2025-12-22 00:40:57 +0100

    Delete redundant plugin
    Accidentally installed it in custom plugins, even though it already existed before in the core config.

--- lua/custom/plugins/gitsigns.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'lewis6991/gitsigns.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  opts = {
-    -- Enable blame information for the current line
-    current_line_blame = true,
-  },
-}
