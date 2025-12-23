2d57e52c2deebc6c33445d5946e2b860d6ad0d61
Tom <rache.bartmoss@toms-macbook-pro.home>
2025-12-23 12:26:30 +0100

    Add ruff language server for formatting

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -211,6 +211,7 @@ return {
         clangd = {},
         gopls = {},
         ty = {},
+        ruff = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
