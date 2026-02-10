84cbaba6a2771d78cec53ddb8114d534cb7f8c4a
Tom Neys <tom.neys@kraken.tech>
2026-02-10 15:51:26 +0000

    Remove copilot plugin
    It started to annoy me and whenever I rely too much on it, I end up debugging silly mistakes.

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -31,9 +31,6 @@ return {
         opts = {},
       },
       'folke/lazydev.nvim',
-      {
-        'giuxtaposition/blink-cmp-copilot',
-      },
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -79,15 +76,9 @@ return {
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer', 'copilot' },
+        default = { 'lsp', 'path', 'snippets', 'lazydev' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-          copilot = {
-            name = 'copilot',
-            module = 'blink-cmp-copilot',
-            score_offset = 100,
-            async = true,
-          },
         },
       },
 
