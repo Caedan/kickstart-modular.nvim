ca8e1ae9a4c7e4265b80feed2861f300dcac04c7
Tom Neys <tomneys@protonmail.com>
2026-05-17 22:11:13 +0100

    Add keybind to search document symbols in Telescope

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -84,6 +84,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sc', builtin.lsp_document_symbols, { desc = '[S]earch document symbols in [C]ode.' })
       vim.keymap.set('n', '<leader>sis', function()
         require('telescope.builtin').live_grep { additional_args = { '--glob', '!tests/*' } }
       end)
