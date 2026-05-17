30fd3dfb315bb113769b710f2f0daf05039eb24b
Tom Neys <tomneys@protonmail.com>
2026-05-17 21:18:42 +0100

    Fix search in source keybind
    This keybind wasn't working correctly. Since, tests in Python are nested
    within the `src/` module, it would fetch the tests as well.

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -85,7 +85,7 @@ return {
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>sis', function()
-        require('telescope.builtin').live_grep { additional_args = { '--glob', 'src/**' } }
+        require('telescope.builtin').live_grep { additional_args = { '--glob', '!tests/*' } }
       end)
 
       -- Slightly advanced example of overriding default behavior and theme
